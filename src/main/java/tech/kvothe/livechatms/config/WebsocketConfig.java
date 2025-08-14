c7609153122b77d50f5a6a8e34ea5cc5c4bb4193
Ítalo H. <italodssouza.backup@gmail.com>
2025-08-14 11:32:19 -0300

    Criando config do websocket

---
+++ src/main/java/tech/kvothe/livechatms/config/WebsocketConfig.java
@@ -0,0 +1,20 @@
+package tech.kvothe.livechatms.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+public class WebsocketConfig implements WebSocketMessageBrokerConfigurer {
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/topics");
+        registry.setApplicationDestinationPrefixes("/app");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/kvothe-livechat-websocket");
+    }
+}
